e8a693a96924535326fd06bd441c2193b627dd2f
Hisham,Ali <his984@gmail.com>
2024-09-23 23:58:50 +0200

    Conditional Rendering With Multiple Returns

--- src/index.js
+++ src/index.js
@@ -82,6 +82,7 @@ function Menu() {
 // pizza Component
 function Pizza(props) {
   // console.log(props);
+  if (props.pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
